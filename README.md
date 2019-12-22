8f7203450a7b600124d5fb06eb215c4bf019f9c2
jakeoeding <jakeoeding>
2019-12-22 12:26:09 -0600

    refactored UI and board code into respective classes

--- README.md
+++ README.md
@@ -3,9 +3,7 @@
 Simple Python implementation of the classic game Connect 4
 
 To do:
-- encapsulate board functionality into Board class
-- encapsulate GUI functionality into Pane class
 - improve game end functionality
-- remove deprecated functions
+- add more documentation
 - add requirements.txt
 - improve readme
--- connect4.py
+++ connect4.py
@@ -1,15 +1,9 @@
-import numpy as np
-import pygame
 import sys
 
+import pygame
 
-# Game constants
-ROW_COUNT = 6
-COLUMN_COUNT = 7
+import board
 
-# UI constants
-SQUARESIZE = 90  # in pixels
-RADIUS = SQUARESIZE // 2 - 5
 
 # Color constants
 BLACK = (0, 0, 0)
@@ -18,88 +12,52 @@ RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 
 
-def create_board():
-    return np.zeros((ROW_COUNT, COLUMN_COUNT))
+class Pane:
+    def __init__(self, row_count, column_count, square_size):
+        self.board = board.Board(row_count, column_count)
+        self.square_size = square_size
+        self.radius = square_size // 2 - 5
+        self.width = column_count * square_size
+        self.height = (row_count + 1) * square_size  # Additional row for next piece at top
+        self.screen = pygame.display.set_mode((self.width, self.height))
 
-def print_board(board):
-    print(np.flip(board, 0))
+    def draw_background(self):
+        for r in range(self.board.row_count):
+            for c in range(self.board.column_count):
+                pygame.draw.rect(self.screen, BLUE, (c * self.square_size, r * self.square_size + self.square_size, self.square_size, self.square_size))
+                pygame.draw.circle(self.screen, BLACK, (c * self.square_size + self.square_size // 2, r * self.square_size + self.square_size + self.square_size // 2), self.radius)
+        pygame.display.update()
 
-def get_player_selection(turn):
-    selection = -1
-    while selection < 1 or selection > COLUMN_COUNT:
-        selection = int(input(f'Player {turn}: Make your selection (1-{COLUMN_COUNT}): '))
-    # Adjust user input by 1 to account for 0 based arrays
-    return selection - 1 
+    def fill_in_pieces(self):
+        for r in range(self.board.row_count):
+            for c in range(self.board.column_count):
+                current_color = BLACK
+                if self.board.grid[r][c] == 1:
+                    current_color = RED
+                elif self.board.grid[r][c] == 2:
+                    current_color = YELLOW
+                pygame.draw.circle(self.screen, current_color, (c * self.square_size + self.square_size // 2, self.height - (r * self.square_size + self.square_size // 2)), self.radius)
+        pygame.display.update()
 
-def is_valid_location(board, column):
-    # Check if last row in column is empty
-    return board[ROW_COUNT - 1][column] == 0
+    def track_mouse_motion(self, x_position, current_color):
+        pygame.draw.rect(self.screen, BLACK, (0, 0, self.width, self.square_size))
+        pygame.draw.circle(self.screen, current_color, (x_position, self.square_size // 2), self.radius)
+        pygame.display.update()
 
-def get_next_open_row(board, column):
-    # Return first instance where row is empty
-    for row in range(ROW_COUNT):
-        if board[row][column] == 0:
-            return row
-
-def drop_piece(board, row, column, turn):
-    board[row][column] = turn
-
-def is_winning_move(board, turn):
-    # Check horizontally
-    for r in range(ROW_COUNT):
-        for c in range(COLUMN_COUNT - 3):
-            if board[r][c] == turn and board[r][c + 1] == turn and board[r][c + 2] == turn and board[r][c + 3] == turn:
-                return True
-
-    # Check vertically
-    for r in range(ROW_COUNT - 2):
-        for c in range(COLUMN_COUNT):
-            if board[r][c] == turn and board[r + 1][c] == turn and board[r + 2][c] == turn and board[r + 3][c] == turn:
-                return True
-
-    # Check diagonally upward
-    for r in range(ROW_COUNT - 3):
-        for c in range(COLUMN_COUNT - 3):
-            if board[r][c] == turn and board[r + 1][c + 1] == turn and board[r + 2][c + 2] == turn and board[r + 3][c + 3] == turn:
-                return True
-    
-    # Check diagonally downward
-    for r in range(3, ROW_COUNT):
-        for c in range(COLUMN_COUNT - 3):
-            if board[r][c] == turn and board[r - 1][c + 1] == turn and board[r - 2][c + 2] == turn and board[r - 3][c + 3] == turn:
-                return True
-
-    return False
-
-def draw_board(board):
-    # Draw background
-    for r in range(ROW_COUNT):
-        for c in range(COLUMN_COUNT):
-            pygame.draw.rect(screen, BLUE, (c * SQUARESIZE, r * SQUARESIZE + SQUARESIZE, SQUARESIZE, SQUARESIZE))
-            pygame.draw.circle(screen, BLACK, (c * SQUARESIZE + SQUARESIZE // 2, r * SQUARESIZE + SQUARESIZE + SQUARESIZE // 2), RADIUS)
-
-    # Draw filled in slots
-    for r in range(ROW_COUNT):
-        for c in range(COLUMN_COUNT):
-            if board[r][c] == 1:
-                pygame.draw.circle(screen, RED, (c * SQUARESIZE + SQUARESIZE // 2, height - (r * SQUARESIZE + SQUARESIZE // 2)), RADIUS)
-            elif board[r][c] == 2:
-                pygame.draw.circle(screen, YELLOW, (c * SQUARESIZE + SQUARESIZE // 2, height - (r * SQUARESIZE + SQUARESIZE // 2)), RADIUS)
-
-    pygame.display.update()
-
-
-board = create_board()
-game_over = False
-turn = 1
+    def try_drop_piece(self, x_position, turn):
+        column_selection = x_position // self.square_size
+        if self.board.is_valid_location(column_selection):
+            row = self.board.get_next_open_row(column_selection)
+            self.board.drop_piece(row, column_selection, turn)
+            return True
+        return False
+        
 
 pygame.init()
-width = COLUMN_COUNT * SQUARESIZE
-height = (ROW_COUNT + 1) * SQUARESIZE
-size = (width, height)
-screen = pygame.display.set_mode(size)
-draw_board(board)
-pygame.display.update()
+pane = Pane(6, 7, 90)
+pane.draw_background()
+game_over = False
+turn = 1
 
 while not game_over:
 
@@ -111,22 +69,15 @@ while not game_over:
             sys.exit()
 
         elif event.type == pygame.MOUSEMOTION:
-            x_position = event.pos[0]
-            pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARESIZE))
-            pygame.draw.circle(screen, current_color, (x_position, SQUARESIZE // 2), RADIUS)
-            pygame.display.update()
+            pane.track_mouse_motion(event.pos[0], current_color)
 
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            column_selection = event.pos[0] // SQUARESIZE
-
-            if is_valid_location(board, column_selection):
-                row = get_next_open_row(board, column_selection)
-                drop_piece(board, row, column_selection, turn)
-            if is_winning_move(board, turn):
-                game_over = True
-                print_board(board)
-                break
-            draw_board(board)
-            turn = 1 if turn == 2 else 2  # Alternate turn between 1 and 2 each loop
+            if pane.try_drop_piece(event.pos[0], turn):
+                pane.fill_in_pieces()
+                if pane.board.has_four_in_a_row(turn):
+                    game_over = True
+                    pane.board.print_grid()
+                    break
+                turn = 1 if turn == 2 else 2  # Alternate turn between 1 and 2 after each valid selection
 
 print(f'Player {turn} wins!')
